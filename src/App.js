f295572a219403b77ee76e06fb70e56424db6ba0
Jim Bevington <jim.bevington@hotmail.com>
2018-04-25 12:03:37 +0100

    added skeleton game body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Game from './containers/Game';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <Game />
     );
   }
 }
