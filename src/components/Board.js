e813b8567a1ebafde614ff25daecea4bcfaa0b22
Jim Bevington <jim.bevington@hotmail.com>
2018-04-25 13:19:38 +0100

    Added basic styling to cells

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,17 +4,17 @@ import Cell from './Cell';
 const Board = (props) => {
   return (
     <React.Fragment>
-      <div>
+      <div className='row'>
         <Cell id='0' value={props.cellData[0]} />
         <Cell id='1' value={props.cellData[1]} />
         <Cell id='2' value={props.cellData[2]} />
       </div>
-      <div>
+      <div className='row'>
         <Cell id='3' value={props.cellData[3]} />
         <Cell id='4' value={props.cellData[4]} />
         <Cell id='5' value={props.cellData[5]} />
       </div>
-      <div>
+      <div className='row'>
         <Cell id='6' value={props.cellData[6]} />
         <Cell id='7' value={props.cellData[7]} />
         <Cell id='8' value={props.cellData[8]} />
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Cell = (props) => {
-  return <p>a cell</p>
+  return <p className='cell'>{props.value}</p>
 }
 
 export default Cell;
