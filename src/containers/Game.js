250842201f16db742bad1cfbf020fedd755fe71e
Jim Bevington <jim.bevington@hotmail.com>
2018-04-25 12:08:14 +0100

    added constructor to Game

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isWon: false,
+      currentPlayer: this.props.players[0],
+      cells: {} // initialise with this empty
+    }
+  }
 
   render(){
     return <h1>This is the Game</h1>
